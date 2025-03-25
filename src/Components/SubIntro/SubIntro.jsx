eb2f7a0ea4f7fcaf51180f944c67ea68cd9c9ee1
Christian Okafor <okaforchristian08@gmail.com>
2025-03-25 19:56:55 +0100

    Update SubIntro.jsx

--- src/Components/SubIntro/SubIntro.jsx
+++ src/Components/SubIntro/SubIntro.jsx
@@ -7,15 +7,16 @@ const SubIntro = () => {
   return (
     <div className='sec1'>
     <div className='section container'>
-    <p className="subtitle">WE HELP YOU GET THINGS DONE</p>
+    <p className="subtitle">WE CAN HELP YOUR PARISH FLOURISH</p>
     <div className="sec">
       <div className="content">
         
         <h1 className="title">
-          Grace, Love and <span className="highlight">Forgiveness</span> for all.
+          Change, Growth, and <span className="highlight">Eucharist</span> for all.
         </h1>
         <p className="description">
-          Beyond Baptism is an amazing church where you can form a deeper connection with our Lord Jesus.
+        Beyond Baptism transforms lives and revitalizes parishes by fostering encounters with Jesus in the Eucharist, empowering discipleship, and providing pastors with a unique, Eucharist-centered
+        framework for parish growth.
         </p>
         <button className="learnMoreBtn btn"><Alert><a className='btn11'>Learn More</a></Alert></button>
       </div>
